6d9c9f638e8b57a44deaea4bfe1f06ddc53f8725
Sadakalum45 <107994339+Sadakalum45@users.noreply.github.com>
2022-06-22 15:41:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94762554100']
+global.premium = ['94762554100']
+global.ownernomer = '94762554100'
+global.ownername = '🐦dhanuka 10 35'
+global.botname = 'Danu-Danu'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://youtube.com/channel/UCaASTNwSB1jvHW5vbqmWh0A'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
